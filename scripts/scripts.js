16e63c5491ddaf31ca563c48a0d54c3c13c9aeb8
Jeff Foxx <foxx@adobe.com>
2024-07-23 14:46:38 -0400

    page background and header

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -29,6 +29,13 @@ function buildHeroBlock(main) {
   }
 }
 
+function setPageBackground() {
+  const body = document.querySelector('body');
+  var backgroundImg = document.querySelector('meta[name=page-background]').content;
+  backgroundImg = backgroundImg.split('.')[1];
+  body.style.backgroundImage = `url(${backgroundImg}/.webp?width=2000&format=webp&optimize=medium)`;
+}
+
 /**
  * load fonts.css and set a session storage flag
  */
@@ -61,6 +68,7 @@ function buildAutoBlocks(main) {
 // eslint-disable-next-line import/prefer-default-export
 export function decorateMain(main) {
   // hopefully forward compatible button decoration
+  setPageBackground();
   decorateButtons(main);
   decorateIcons(main);
   buildAutoBlocks(main);
